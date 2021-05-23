dee3467c78ddeb2199112c936f976496541bde6d
minho lee <91aiden.lee@gmail.com>
2021-05-23 23:41:25 +0900

    Set template engine

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,17 +1,45 @@
+// Fake data
+const videos = [
+  {
+    title: 'First Video',
+    rating: 5,
+    comments: 2,
+    createdAt: '5 minutes ago',
+    views: 44,
+    id: 1,
+  },
+  {
+    title: 'Second Video',
+    rating: 4,
+    comments: 1,
+    createdAt: '5 minutes ago',
+    views: 21,
+    id: 2,
+  },
+  {
+    title: 'Third Video',
+    rating: 2,
+    comments: 0,
+    createdAt: '15 minutes ago',
+    views: 12,
+    id: 3,
+  },
+];
+
 export const home = (req, res) => {
-  res.send('Home');
+  res.render('home', { pageTitle: 'Home', videos });
+};
+
+export const see = (req, res) => {
+  res.render('watch', { pageTitle: 'Watch' });
+};
+
+export const edit = (req, res) => {
+  res.render('edit', { pageTitle: 'Edit Video' });
 };
 
 export const search = (req, res) => {
   res.send('Search');
-};
-
-export const see = (req, res) => {
-  res.send(`Watch Video #${req.params.id}`);
-};
-
-export const edit = (req, res) => {
-  res.send('Edit Video');
 };
 
 export const upload = (req, res) => {
