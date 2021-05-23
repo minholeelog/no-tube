2be789243388a9d99572ca755987356b9f124909
minho lee <91aiden.lee@gmail.com>
2021-05-24 01:06:51 +0900

    New feature: Upload video data

--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -1,10 +1,10 @@
 import express from 'express';
 import {
-  deleteVideo,
   watch,
-  upload,
   getEdit,
   postEdit,
+  getUpload,
+  postUpload,
 } from '../controllers/videoController';
 
 const videoRouter = express.Router();
@@ -12,5 +12,6 @@ const videoRouter = express.Router();
 videoRouter.get('/:id(\\d+)', watch);
 // 동일한 URL에 get, post method를 사용할 경우..
 videoRouter.route('/:id(\\d+)/edit').get(getEdit).post(postEdit);
+videoRouter.route('/upload').get(getUpload).post(postUpload);
 
 export default videoRouter;
