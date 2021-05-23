a33c7956d107bbebf1ea60709fcc7d1e38a94833
minho lee <91aiden.lee@gmail.com>
2021-05-24 00:41:46 +0900

    New feature: Edit video title

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -30,22 +30,21 @@ export const home = (req, res) => {
   res.render('home', { pageTitle: 'Home', videos });
 };
 
-export const see = (req, res) => {
-  res.render('watch', { pageTitle: 'Watch' });
+export const watch = (req, res) => {
+  const { id } = req.params;
+  const video = videos[id - 1];
+  return res.render('watch', { pageTitle: `Watching: ${video.title}`, video });
 };
 
-export const edit = (req, res) => {
-  res.render('edit', { pageTitle: 'Edit Video' });
+export const getEdit = (req, res) => {
+  const { id } = req.params;
+  const video = videos[id - 1];
+  return res.render('edit', { pageTitle: `Editing: ${video.title}`, video });
 };
 
-export const search = (req, res) => {
-  res.send('Search');
-};
-
-export const upload = (req, res) => {
-  res.send('Upload Video');
-};
-
-export const deleteVideo = (req, res) => {
-  res.send('Delete Video');
+export const postEdit = (req, res) => {
+  const { id } = req.params;
+  const { title } = req.body;
+  videos[id - 1].title = title;
+  return res.redirect(`/videos/${id}`);
 };
--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -1,11 +1,16 @@
 import express from 'express';
-import { deleteVideo, edit, see, upload } from '../controllers/videoController';
+import {
+  deleteVideo,
+  watch,
+  upload,
+  getEdit,
+  postEdit,
+} from '../controllers/videoController';
 
 const videoRouter = express.Router();
 
-videoRouter.get('/:id(\\d+)', see);
-videoRouter.get('/:id(\\d+)/edit', edit);
-videoRouter.get('/:id(\\d+)/delete', deleteVideo);
-videoRouter.get('/upload', upload);
+videoRouter.get('/:id(\\d+)', watch);
+// 동일한 URL에 get, post method를 사용할 경우..
+videoRouter.route('/:id(\\d+)/edit').get(getEdit).post(postEdit);
 
 export default videoRouter;
