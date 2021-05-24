361c4824588960ca668bc1c69d0024cb4ecf5e2f
minho lee <91aiden.lee@gmail.com>
2021-05-24 23:56:22 +0900

    Get video from DB

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,51 +1,31 @@
-// Fake data
-const videos = [
-  {
-    title: 'First Video',
-    rating: 5,
-    comments: 2,
-    createdAt: '5 minutes ago',
-    views: 44,
-    id: 1,
-  },
-  {
-    title: 'Second Video',
-    rating: 4,
-    comments: 1,
-    createdAt: '5 minutes ago',
-    views: 21,
-    id: 2,
-  },
-  {
-    title: 'Third Video',
-    rating: 2,
-    comments: 0,
-    createdAt: '15 minutes ago',
-    views: 12,
-    id: 3,
-  },
-];
+import Video from '../models/Video';
 
-export const home = (req, res) => {
-  res.render('home', { pageTitle: 'Home', videos });
+export const home = async (req, res) => {
+  // callback
+  /*
+  Video.find({}, (error, videos) => {
+    return res.render('home', { pageTitle: 'Home', videos: [] });
+  });
+  */
+
+  // async, await
+  const videos = await Video.find({});
+  return res.render('home', { pageTitle: 'Home', videos });
 };
 
 export const watch = (req, res) => {
   const { id } = req.params;
-  const video = videos[id - 1];
-  return res.render('watch', { pageTitle: `Watching: ${video.title}`, video });
+  return res.render('watch', { pageTitle: `Watching` });
 };
 
 export const getEdit = (req, res) => {
   const { id } = req.params;
-  const video = videos[id - 1];
-  return res.render('edit', { pageTitle: `Editing: ${video.title}`, video });
+  return res.render('edit', { pageTitle: `Editing` });
 };
 
 export const postEdit = (req, res) => {
   const { id } = req.params;
   const { title } = req.body;
-  videos[id - 1].title = title;
   return res.redirect(`/videos/${id}`);
 };
 
@@ -55,14 +35,6 @@ export const getUpload = (req, res) => {
 
 export const postUpload = (req, res) => {
   const { title } = req.body;
-  const newVideo = {
-    title,
-    rating: 0,
-    comments: 0,
-    createdAt: 'Just now',
-    views: 0,
-    id: videos.length + 1,
-  };
-  videos.push(newVideo);
+
   return res.redirect('/');
 };
--- src/server.js
+++ src/server.js
@@ -1,12 +1,9 @@
-import './db';
-import './models/Video';
 import express from 'express';
 import logger from 'morgan';
 import globalRouter from './routers/globalRouter';
 import userRouter from './routers/userRouter';
 import videoRouter from './routers/videoRouter';
 
-const PORT = 4040;
 const app = express();
 
 app.set('view engine', 'pug');
@@ -17,7 +14,4 @@ app.use('/', globalRouter);
 app.use('/users', userRouter);
 app.use('/videos', videoRouter);
 
-const handleListening = () =>
-  console.log(`✅ Server listening on: http://localhost:${PORT}`);
-
-app.listen(PORT, handleListening);
+export default app;
