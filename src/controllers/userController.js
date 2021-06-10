ba52019007e6e371cc13574e9a8553189c9d8ddb
minho lee <91aiden.lee@gmail.com>
2021-06-11 00:09:17 +0900

    Handle status code

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -8,7 +8,7 @@ export const postJoin = async (req, res) => {
   const pageTitle = 'Join';
   const { name, username, email, password, password2, location } = req.body;
   if (password !== password2) {
-    return res.render('join', {
+    return res.status(400).render('join', {
       pageTitle,
       errorMessage: 'Password confirmation does not match.',
     });
@@ -16,7 +16,7 @@ export const postJoin = async (req, res) => {
 
   const exists = await User.exists({ $or: [{ username }, { email }] });
   if (exists) {
-    res.render('join', {
+    res.status(400).render('join', {
       pageTitle,
       errorMessage: 'This username/email is already taken.',
     });
